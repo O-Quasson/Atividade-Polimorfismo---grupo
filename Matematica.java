e89d0a69d0998a949d93fb99157ebab632126eb5
Gabriel <kenddygabriel841@gmail.com>
2025-05-07 12:11:13 -0300

    Update Matematica.java

--- Matematica.java
+++ Matematica.java
@@ -1,5 +1,3 @@
-import java.util.Random;
-
 public class Matematica extends Materia{
 
     public Matematica(String questao, String resp){
@@ -8,17 +6,12 @@ public class Matematica extends Materia{
 
     @Override
     public void exibirExplicacao(int random){
-        if(random==1){
+        if(random==0){
             System.out.println("Vai tomar no cu");
-        }else if(random==2){
+        }else if(random==1){
             System.out.println("Vai tomar no cu 2");
-        }else if(random==3){
+        }else if(random==2){
             System.out.println("Vai tomar no cu 3");
         }
     };
 }
-
-
-
-
-
